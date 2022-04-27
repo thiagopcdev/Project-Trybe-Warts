30bd94b270d298550af3a4d7240cfff00450e730
Thiago Prado <thiagopc_dev@hotmail.com>
2022-04-27 18:56:53 -0300

    Project Trybe-Warts

---
+++ README.md
@@ -0,0 +1,61 @@
+## Boas vindas ao repositório do projeto Trybewarts Wizarding School! :mage:
+
+### Habilidades
+
+  * Criar formulários em HTML;
+
+  * Utilizar CSS Flexbox para criar layouts flexíveis;
+
+  * Criar regras CSS específicas para serem aplicadas a dispositivos móveis;
+
+  * Construir páginas que alteram o seu layout de acordo com a orientação da tela;
+
+---
+
+Neste projeto, desenvolvi uma página de formulário da Escola de Magia de Trybewarts, em que as pessoas estudantes poderão enviar seus feedbacks sobre a escola. O tema desse projeto é baseado na obra 'Harry Potter', de J. K. Rowling, já que programar é o mais próximo que podemos chegar de algo *verdadeiramente mágico*!
+
+---
+
+### Requisitos do projeto
+
+#### 1. Crie uma barra verde na parte superior da página
+
+#### 2. Adicione o logotipo da Trybewarts com a classe `trybewarts-header-logo` no canto esquerdo da barra superior
+
+#### 3. Acrescente um formulário de autenticação no canto direito da barra superior contendo os inputs de login, de senha e um botão de entrar
+
+#### 4. Crie um título com o texto 'Trybewarts' centralizado dentro do 'Header'
+
+#### 5. Adicione um formulário no corpo da página, posicionado ao lado esquerdo
+
+#### 6. Faça com que o formulário do requisito 5 seja um container flex com o eixo principal vertical
+
+#### 7. Adicione a logo da Trybewarts no lado direito da página
+
+#### 8.Acrescente no formulário os inputs de 'Nome:', 'Sobrenome:' e 'Email:'
+
+#### 9. Crie um select 'Casa' contendo quatro options
+
+#### 10. Alinhe os campos de 'Nome' e 'Sobrenome' para que fiquem em linha
+
+#### 11. Alinhe os campos de 'Email' e 'Casa' para que fiquem em linha
+
+#### 12. Crie um campo de entrada para qual família a pessoa estudante se identifica
+
+#### 13. Crie campos de entrada do tipo 'checkbox' contendo seis opções
+
+#### 14. Crie campo de entrada para avaliar de 1 a 10 o nível de satisfação com a Trybewarts
+
+#### 15. Crie uma textarea com o id 'textarea' e uma label com a classe 'textarea' contendo o número máximo de caracteres igual à 500
+
+#### 16. Crie um campo de entrada do tipo 'checkbox' com o id 'agreement' para validar as informações
+
+#### 17. Crie um botão de Enviar para submeter o formulário
+
+#### 18. Faça com que o botão 'Enviar' seja habilitado somente após a checkbox do requisito 16 ser selecionada
+
+#### 19. Crie um rodapé no final da página
+
+#### 20. Crie um contador com o ID 'counter' contendo o número de caracteres disponíveis no textarea, variando de 500 até 0, que deverá ser atualizado a medida que algo for digitado na textarea
+
+#### 21. Faça com que ao clicar no botão 'Enviar', o conteúdo do formulário seja substituído pelas informações preenchidas
---
+++ script.js
@@ -0,0 +1,145 @@
+
+function getLogin() {
+  const login = document.getElementById('input-login').value;
+  return login;
+}
+
+function getSenha() {
+  const senha = document.getElementById('input-senha').value;
+  return senha;
+}
+
+function validateLogin(login, senha) {
+  if ((login === 'tryber@teste.com') && (senha === '123456')) return true;
+}
+
+function loginButton() {
+  const button = document.getElementById('btt-login');
+  button.addEventListener('click', () => {
+    if (validateLogin(getLogin(), getSenha())) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
+
+function getAgreement() {
+  const agreement = document.getElementById('agreement').checked;
+  return agreement;
+}
+
+function enableButton() {
+  const agreement = document.getElementById('agreement');
+  const buttonCheck = document.getElementById('submit-btn');
+  agreement.addEventListener('input', () => {
+    if (getAgreement()) {
+      buttonCheck.disabled = false;
+    } else {
+      buttonCheck.disabled = true;
+    }
+  });
+}
+
+function textCont() {
+  const maxChar = document.getElementById('counter');
+  const inputText = document.getElementById('textarea');
+  const valorMax = 500;
+  inputText.addEventListener('input', (e) => {
+    const textSise = e.target.value.length;
+    const total = valorMax - textSise;
+    maxChar.innerHTML = total;
+  });
+}
+
+const originalFamilyId = [];
+
+function familyList() {
+  const familyId = document.querySelectorAll('.family');
+  for (let index = 0; index < familyId.length; index += 1) {
+    originalFamilyId.push(familyId[index].id);
+  }
+}
+
+function familyChangeId() {
+  const familyName = document.querySelectorAll('.family');
+  for (let index = 0; index < familyName.length; index += 1) {
+    if (familyName[index].checked) {
+      familyName[index].id = 'family-selected';
+    }
+    if (!familyName[index].checked) {
+      familyName[index].id = originalFamilyId[index];
+    }
+  }
+}
+
+const originalrateListId = [];
+
+function rateChangeId() {
+  const rateId = document.querySelectorAll('.form-check-input');
+  for (let index = 0; index < rateId.length; index += 1) {
+    if (rateId[index].checked) {
+      rateId[index].id = 'rate-selected';
+    }
+    if (!rateId[index].checked) {
+      rateId[index].id = originalrateListId[index];
+    }
+  }
+}
+
+const originalSubjectsId = [];
+
+function subjectList() {
+  const allSubjects = document.querySelectorAll('.subject');
+  for (let index = 0; index < allSubjects.length; index += 1) {
+    if (allSubjects[index].checked) {
+      originalSubjectsId.push(allSubjects[index].value);
+    }
+  }
+}
+
+function getData() {
+  const data = {
+    name: document.getElementById('input-name').value,
+    lastName: document.getElementById('input-lastname').value,
+    email: document.getElementById('input-email').value,
+    house: document.getElementById('house').value,
+    family: document.getElementById('family-selected').value,
+    lessons: originalSubjectsId,
+    avaliation: document.getElementById('rate-selected').value,
+    observation: document.getElementById('textarea').value,
+  };
+  return data;
+}
+
+function createLi(innerhtml) {
+  const li = document.createElement('li');
+  li.innerHTML = innerhtml;
+  return li;
+}
+
+function checkButton() {
+  const button = document.getElementById('submit-btn');
+  button.addEventListener('click', (e) => {
+    familyChangeId();
+    subjectList();
+    rateChangeId();
+    e.preventDefault();
+    const newDiv = document.querySelector('#show-result ul');
+    newDiv.appendChild(createLi(`Nome: ${getData().name} ${getData().lastName}`));
+    newDiv.appendChild(createLi(`Email: ${getData().email}`));
+    newDiv.appendChild(createLi(`Casa: ${getData().house}`));
+    newDiv.appendChild(createLi(`Família: ${getData().family}`));
+    newDiv.appendChild(createLi(`Matérias: ${getData().lessons.join(', ')}`));
+    newDiv.appendChild(createLi(`Avaliação: ${getData().avaliation}`));
+    newDiv.appendChild(createLi(`Observações: ${getData().observation}`));
+  });
+}
+
+window.onload = () => {
+  loginButton();
+  enableButton();
+  textCont();
+  familyList();
+  checkButton();
+};
